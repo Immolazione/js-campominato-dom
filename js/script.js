d1b2f1426ab065c61fdee49bf064ead9ad7ecaa3
Immolazione <federico.deered@gmail.com>
2022-01-24 19:37:09 +0100

    restart

--- js/script.js
+++ js/script.js
@@ -1,72 +1,23 @@
 console.log('JS OK');
 
-const generateButton = document.getElementById('generate');
-const grid = document.getElementById('grid');
+/*
+L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
 
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
 
+BONUS:
+1- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
+2- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
 
-function play() {
-    // SVUOTA GRID E BOTTONE
-    generateButton.innerText = 'Ricomincia';
-    grid.innerText = '';
+*/
 
-    // FASE PREPARATIVA
-    // BOMBE
-    const TOTAL_BOMBS = 16;
+// FUNZIONE RANDOMIZE
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-    // RECUPERO VALORI TENDINA
-
-    const level = parseInt(document.getElementById('number-of-cells').value);
-    console.log(level);
-    let col = Math.sqrt(level);
-    let cell;
-    let points = 0;
-    console.log(col);
-
-    function generateCell (cellInRow, i) {
-        cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.style.width = `calc(100% / ${cellInRow})`;
-        cell.style.height = `calc(100% / ${cellInRow})`;
-        const number = i + 1;
-        cell.id = number;
-        cell.innerText = number;
-        return cell;
-    };
-
-    const getrandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-    //! Funzione che mi crea un array di 16 numeri casuali
-    const generateBombs = (totalBombs, numberOfElement) => {
-        const bombs = [];
-        while (bombs.length < totalBombs) {
-            const randomNumber = getrandomNumber(1, numberOfElement);
-            if (!bombs.includes(randomNumber)) {
-                bombs.push(randomNumber);
-            };
-        };
-        return bombs;
-    };
-    const bombs = generateBombs(TOTAL_BOMBS, level);
-    console.log(bombs);
-    
-
-
-    for (let i = 0; i < level; i++) {
-        const cell = generateCell(col, i);
-        cell.addEventListener ('click', (e) => onCellClick(e.target, bombs, i));
-        grid.appendChild(cell);
-        console.log(i);
-        const onCellClick = (cell) => {
-            if (bombs.includes(i)) {
-                cell.classList.add('bomb');
-            } else {
-                cell.classList.add('clicked');
-            };
-        };
-      
-    };
-};
-
-
-generateButton.addEventListener('click', play);
